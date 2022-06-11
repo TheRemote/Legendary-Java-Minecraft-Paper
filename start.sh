6395ce98e668d8edce44e9b5c30ef522e5fb7b16
James Chambers <05jchambers@gmail.com>
2022-06-11 16:09:54 -0600

    Update to 1.19 (make backups first!)

--- start.sh
+++ start.sh
@@ -109,6 +109,10 @@ else
     # Get latest build
     BuildJSON=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4.212 Safari/537.36" https://papermc.io/api/v2/projects/paper/versions/$Version)
     Build=$(echo "$BuildJSON" | rev | cut -d, -f 1 | cut -d] -f 2 | rev)
+    # Fix for if there is only one build in the branch
+    if [ -n $(echo "$Build" | grep builds)]; then
+        Build=0
+    fi
     Build=$(($Build + 0))
     if [[ $Build != 0 ]]; then
         echo "Latest paperclip build found: $Build"
