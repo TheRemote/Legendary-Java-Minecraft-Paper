3e2c601f098ae47d204359f9c4eb4386478da637
James A. Chambers <05jchambers@gmail.com>
2022-10-30 15:46:21 -0600

    Fix bug with daily scheduled restart

--- start.sh
+++ start.sh
@@ -66,6 +66,8 @@ fi
 
 # Daily scheduled restart
 if [ -z "$ScheduleRestart" ]; then
+    echo "No daily restart scheduled"
+else
     FutureRestart=$(shutdown -r "$ScheduleRestart")
     echo "Scheduling daily restart: $FutureRestart"
 fi
