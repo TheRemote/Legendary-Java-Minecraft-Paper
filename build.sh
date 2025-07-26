fdd109d139de7dbb08f4e9c66c81ac459439a229
James Chambers <05jchambers@gmail.com>
2025-07-26 13:37:40 -0600

    Update build process to buildx

--- build.sh
+++ build.sh
@@ -1,16 +1,1 @@
-docker build -t 05jchambers/legendary-java-minecraft-paper:amd64 -f amd64.Dockerfile .
-docker push 05jchambers/legendary-java-minecraft-paper:amd64
-
-docker build -t 05jchambers/legendary-java-minecraft-paper:arm64v8 -f arm64v8.Dockerfile .
-docker push 05jchambers/legendary-java-minecraft-paper:arm64v8
-
-docker build -t 05jchambers/legendary-java-minecraft-paper:armv7 -f armv7.Dockerfile .
-docker push 05jchambers/legendary-java-minecraft-paper:armv7
-
-docker build -t 05jchambers/legendary-java-minecraft-paper:ppc64le -f ppc64le.Dockerfile .
-docker push 05jchambers/legendary-java-minecraft-paper:ppc64le
-
-docker build -t 05jchambers/legendary-java-minecraft-paper:s390x -f s390x.Dockerfile .
-docker push 05jchambers/legendary-java-minecraft-paper:s390x
-
-./manifest-tool-linux-amd64 push from-spec multi-arch-manifest.yaml
+docker buildx build --sbom=true --provenance=true --platform linux/amd64,linux/arm64,linux/arm/v7,linux/riscv64,linux/s390x,linux/ppc64le --tag 05jchambers/legendary-java-minecraft-paper:latest --push -f Build.Dockerfile .
